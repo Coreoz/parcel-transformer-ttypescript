03af56cf66db80e0d15d00e3c65b4b05b43ec24f
Aurélien Manteaux <amanteaux@coreoz.com>
2021-03-16 13:46:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Parcel Transformer Ttypescript
+==============================
+A replacement of the standard [Parcel v2 transformer Typescript](https://v2.parceljs.org/languages/typescript/#typescript's-tsc)
+to use [ttypescript](https://github.com/cevek/ttypescript).
+This enables to use easily [custom TypeScript transformers](https://dev.doctorevidence.com/how-to-write-a-typescript-transform-plugin-fc5308fdd943) inside [Parcel 2](https://v2.parceljs.org/) projects.
+
+Usage
+-----
+Inside the `.parcelrc` file, reference the transformer:
+```json
+{
+  "extends": "@parcel/config-default",
+  "transformers": {
+    "*.{ts,tsx}": ["parcel-transformer-ttypescript"]
+  }
+}
+```
+
+In the `tsconfig.json`, reference your TypeScript transformers:
+```json
+{
+  "compilerOptions": {
+    "plugins": [
+      {"transform": "./build-scripts/di-transformer-adapter.ts" },
+      {"transform": "ts-transformer-classname" }
+    ]
+  }
+}
+```
---
+++ src/TtscTransformer.ts
@@ -0,0 +1,41 @@
+import { Transformer } from '@parcel/plugin';
+import { loadTSConfig } from '@parcel/ts-utils';
+import typescript from 'ttypescript';
+
+const TtscTransformer = new Transformer({
+  async loadConfig({config, options}) {
+    await loadTSConfig(config, options);
+  },
+
+  async transform({asset, config}) {
+    asset.type = 'js';
+
+    let code = await asset.getCode();
+    let transpiled = typescript.transpileModule(
+      code,
+      {
+         compilerOptions: {
+           // React is the default. Users can override this by supplying their own tsconfig,
+           // which many TypeScript users will already have for typechecking, etc.
+           jsx: typescript.JsxEmit.React,
+           ...config,
+           // Always emit output
+           noEmit: false,
+           // Don't compile ES `import`s -- scope hoisting prefers them and they will
+           // otherwise compiled to CJS via babel in the js transformer
+           module: typescript.ModuleKind.ESNext,
+         },
+         fileName: asset.filePath, // Should be relativePath?
+       },
+    );
+
+    return [
+      {
+        type: 'js',
+        content: transpiled.outputText,
+      },
+    ];
+  },
+});
+
+export default TtscTransformer;
